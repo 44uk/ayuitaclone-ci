91c2044a4dafb37ddeb58b6ed3394250900c63d7
yukku0423 <yukku0423@gmail.com>
2011-10-06 08:40:21 +0900

    mod

---
+++ application/views/items.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>ayuita-clone</title>
+  <link rel="stylesheet" href="ayuitaclone.css" />
+  <script type="text/javascript" src="ayuitaclone.js"></script>
+</head>
+<body>
+
+<h1>AyuIta-Clone</h1>
+
+<ul>
+  <li>VIP認証</li>
+  <li>新規提供品POST</li>
+  <li>過去ログ一覧</li>
+  <li>管理モード</li>
+</ul>
+
+<ul>
+  <li>要書込のものは追加書き込みをすることでアドレスが現れます。</li>
+  <li>UP職人はD/Lできる人数を設定できます。設定人数を超えると提供品のアドレスは表示されません。</li>
+  <li>優秀UP職人として活動していただいた方にはVIP特典が贈られます。</li>
+</ul>
+
+<table>
+  <tr>
+    <th>提供日</th>
+    <th>UP職人</th>
+    <th>提供品</th>
+    <th>種別</th>
+    <th>DL数</th>
+    <th>トピック状況</th>
+    <th>削除</th>
+  </tr>
+  <tr>
+    <td>01/02</td>
+    <td>名無し職人</td>
+    <td><a href="#">何かいいもの</a></td>
+    <td>テキスト</td>
+    <td>0</td>
+    <td>無制限</td>
+    <td><input type="button" value="削除" /></td>
+  </tr>
+  <tr>
+    <td>01/01</td>
+    <td>名無し職人</td>
+    <td><a href="#">何かいいもの</a></td>
+    <td>音楽</td>
+    <td>0</td>
+    <td>残り<span class="limit">30</span>人</td>
+    <td><input type="button" value="削除" /></td>
+  </tr>
+</table>
+
+<h3>過去ログ一覧</h3>
+<ul>
+  <li>2011-4</li>
+  <li>2011-3</li>
+  <li>2011-2</li>
+  <li>2011-1</li>
+</ul>
+
+</body>
+</html>
